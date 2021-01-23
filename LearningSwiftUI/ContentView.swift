7cd4b8282cc746b2ba88595678d181fa627ce3b3
AlexanderMarchant <alexmarchant@Alexs-MacBook-Pro.local>
2021-01-23 13:32:17 +0000

    Initial Commit

---
+++ LearningSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningSwiftUI
+//
+//  Created by Alex Marchant on 23/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
